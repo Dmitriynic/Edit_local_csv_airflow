944ead146103a510fcacc55536af53cf0ce66e13
root <root@debian.org>
2024-04-20 15:55:18 -0400

    edit local csv

---
+++ dags/data_cleaning_dag/data_cleaning.py
@@ -0,0 +1,55 @@
+from datetime import timedelta #Required to calculate duration
+import airflow #Required to instantiate a DAG
+import pandas as pd
+import numpy as np
+import os
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator #Python operator will be used in our task to run any Python code
+
+dag_path = os.getcwd()
+
+def data_cleaning():
+    hotel_data = pd.read_csv("raw_data/hotel_booking.csv")
+    hotel_data.head()
+    hotel_data.info()
+    hotel_data.describe()
+
+    hotel_data.isnull().sum()
+
+    nan_replacements = {'children': 0, 'country': 'Unknown', 'agent': 'Organic Booking',
+                        'company': 'Personal Booking'}
+    cleaned_data = hotel_data.fillna(nan_replacements)
+
+    cleaned_data.info()
+
+    cleaned_data.to_csv("processed_data/processed_hotel_data.csv", index=False)
+
+def cleaned_data_message():
+    print('Data successfully cleaned')
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(7),
+}
+
+data_cleaning_dag = DAG(
+    'data_cleaning_dag',
+    default_args = default_args,
+    schedule_interval=timedelta(days=30),
+    catchup=False
+)
+
+clean_data = PythonOperator(
+    task_id='data_cleaning',
+    python_callable=data_cleaning,
+    dag=data_cleaning_dag,
+)
+
+message = PythonOperator(
+    task_id='cleaned_data_message',
+    python_callable=cleaned_data_message,
+    dag=data_cleaning_dag,
+)
+clean_data >> message
